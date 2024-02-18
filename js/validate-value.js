9e2fce61fce183190f60adf80987f3796dfd1115
AndreyTara <explorituse@gmail.com>
2024-02-18 19:58:07 +0200

    adds-comments

--- js/validate-value.js
+++ js/validate-value.js
@@ -1,67 +1,80 @@
+// Функція для перевірки відповідності типу змінної до зазначеного типу
+/**
+ * Генерує повідомлення про замовлення дроїдів з вказаною кількістю та ціною за одиницю.
+ *
+ * @param {any} item - змінна
+ * @param {sttring} type тип змінної
+ * @returns {boolean} - Повідомлення про замовлення дроїдів та загальну вартість.
+ */
 function isCheckTypeOfEl(obj) {
+  // Деструктуризація об'єкта obj, витягуємо значення el і elType
   const { el: item, elType: type } = obj;
-  // console.log(item, type);
-  // item - змінна
-  // typeOfItem - перевірка відповідності типу змінній
 
+  // Ініціалізація змінної isUse, яка визначає, чи відповідає тип змінної типу
   let isUse;
+
+  // Перевірка, чи є змінна item об'єктом та тип type - "object"
   const isObjItem = typeof item === "object";
   const isObjType = type === "object";
 
+  // Перевірка, чи є змінна item масивом, тип type - "array" і змінна item є об'єктом
   const isArrayItem = Array.isArray(item);
   const isArrayType = type === "array" && isObjItem;
 
+  // Перевірка, чи є змінна item функцією та тип type - "function"
   const isFnItem = typeof item === "function";
   const isFnType = type === "function";
 
+  // Перевірка, чи є змінна item рядком та тип type - "string"
   const isStrItem = typeof item === "string";
   const isStrType = type === "string";
 
+  // Перевірка, чи є змінна item числом, тип type - "number"
   const isNumItem = typeof item === "number";
   const isNumType = type === "number";
 
-  const isNanItem = isNaN(item);
+  // Перевірка, чи змінна item є NaN, тип type - "nan"
+  const isNanItem = Number.isNaN(item);
   const isNanType = type === "nan";
 
+  // Отримання фактичного типу змінної item
   const currentTypeOfEl = typeof item;
-  const strWrongNumber = `The value of variable ${item} of type "${currentTypeOfEl}" does not correspond to type "${type}".`;
 
+  // Повідомлення про помилку для невідповідності типу
+  const strWrongMessage = `The value of variable ${item} of type "${currentTypeOfEl}" does not correspond to type "${type}".`;
+
+  // Використання оператора switch для визначення відповідності типу
   switch (true) {
     case isFnType && isFnItem:
-      // console.log("isFn");
       isUse = true;
       return isUse;
 
     case isArrayType && isArrayItem:
-      // console.log("isArray");
       isUse = true;
       return isUse;
 
     case isObjType && isObjItem:
-      // console.log("isObj");
       isUse = true;
       return isUse;
 
     case isStrType && isStrItem:
-      // console.log("isStr");
       isUse = true;
       return isUse;
 
     case !isNanItem && isNumType && isNumItem:
-      // console.log("number");
       isUse = true;
       return isUse;
 
     case isNanItem && isNanType:
-      // console.log("isNaN");
       isUse = true;
       return isUse;
 
     default:
-      console.log(strWrongNumber);
+      console.log(strWrongMessage);
       isUse = false;
       return isUse;
   }
 }
-function asd() {}
-console.log(isCheckTypeOfEl({ el: 10, elType: "number" }));
+
+// Приклад використання функції
+console.log(isCheckTypeOfEl({ el: "xc12", elType: "number" }));
